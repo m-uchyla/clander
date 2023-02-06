3dcdc59b688c597a896f4c247ced8fa695c941c5
M. Uchyła <czarnytroll@gmail.com>
2023-02-06 10:32:18 +0100

    init commit

---
+++ src/main/java/com/fdmgroup/clander/ClanderApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.clander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/clander/ClanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.clander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
